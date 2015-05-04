86d22ea5bf84d2fd1187999599e9c477306569c2
ggmenendez <g.gmenendez@alumnos.upm.es>
2015-05-04 22:02:42 +0200

    models.js cambió - falta la DB

--- models/models.js
+++ models/models.js
@@ -1,10 +1,27 @@
 var path = require('path');
 
+// Postgres DATABASE_URL
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar modelo ORM
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null, null, null,
-                             {dialect: "sqlite", storage: "quiz.sqlite"}
+var sequelize = new Sequelize(DB_name, user, pwd,
+                              { dialect: protocol,
+                                protocol: protocol,
+                                port: port,
+                                host: host,
+                                storage: storage,   // solo SQLite (.env)
+                                omitNull: true      // solo Postgres
+                              }
                              );
 
 // Importar la definición de la tabla Quiz en quiz.js
@@ -13,7 +30,7 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; //Exportar definición de la tabla Quiz
 
 // Crear e inicializar la tabla de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
     Quiz.count().success(function(count){
         if (count == 0) {
             Quiz.create({ pregunta: 'Capital de Italia',
