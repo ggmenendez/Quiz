232f3c1354bf9e47b2869608233a6c7efb4b435e
ggmenendez <g.gmenendez@alumnos.upm.es>
2015-05-04 21:27:37 +0200

    Iniciada base de datos

---
+++ models/models.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+// Cargar modelo ORM
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(null, null, null,
+                             {dialect: "sqlite", storage: "quiz.sqlite"}
+                             );
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; //Exportar definición de la tabla Quiz
+
+// Crear e inicializar la tabla de preguntas en DB
+sequelize.sync().success(function() {
+    Quiz.count().success(function(count){
+        if (count == 0) {
+            Quiz.create({ pregunta: 'Capital de Italia',
+                          respuesta: 'Roma'
+                        })
+            .success(function(){ console.log('Base de datos inicializada')});
+        };
+    });
+});
