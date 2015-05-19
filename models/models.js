2b5ee35d44096440a64158602a884bc0a9619829
Pablo Aznar <pablo.aznard@alumnos.upm.es>
2015-05-19 17:48:31 +0200

    Comentarios

--- models/models.js
+++ models/models.js
@@ -27,7 +27,14 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import(quiz_path);
 
+var comment_path = path.join(__dirname, 'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; //Exportar definición de la tabla Quiz
+exports.Comment = Comment;
 
 // Crear e inicializar la tabla de preguntas en DB
 sequelize.sync().then(function() {
